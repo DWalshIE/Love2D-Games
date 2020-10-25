a7bc05a93b8a4457ecd7be9df569dfbbca71a3c9
dylan <dylanw2u19@gmail.com>
2020-10-25 17:48:24 +0000

    Created shootingGallery folder and completed labs

---
+++ ShootingGallery/loadUpdateDrawFunctions/main.lua
@@ -0,0 +1,19 @@
+-- runs when game loads
+-- major variables declared here
+function love.load()
+    
+end
+
+--[[ variables altered here
+     dt stands for delta time
+     is our game loop, gets called every frame our game runs at --]]
+function love.update(dt)
+     
+end
+
+
+-- Reserved for graphics/images
+-- Don't declare calculations/important vars here
+function love.draw()
+
+end
