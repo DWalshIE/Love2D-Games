221468e35d191a0c8eaaea626c05eb1567cb48d2
dylan <dylanw2u19@gmail.com>
2020-10-25 16:06:30 +0000

    Fixed main class

--- main.lua
+++ main.lua
@@ -2,10 +2,6 @@ message = 5
 chicken = "10"
 output = chicken * 3
 
-
-message = message - 2
-message
-message = "this just a test"
 function love.draw()
     love.graphics.setFont(love.graphics.newFont(50))
     love.graphics.print(message)
