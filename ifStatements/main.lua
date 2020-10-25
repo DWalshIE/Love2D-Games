59742c768e50fd0c61c97d14ef1e0f496340f24f
dylan <dylanw2u19@gmail.com>
2020-10-25 16:24:36 +0000

    Completed elseif

--- ifStatements/main.lua
+++ ifStatements/main.lua
@@ -2,14 +2,23 @@ message = 0
 condition = -25
 
 
+--[[
 if condition > 0 then
     message = 1
-end
-
-if condition <= 0 then
+else
     message = -1
-end
+end--]]
 
+-- else if 
+if condition > 0 then
+    message = 1
+elseif condition < -100 then
+    message = -1
+elseif condition == -25 then
+    message = "hello!"
+else
+    message = "No conditions met!"
+end
 
 
 function love.draw()
