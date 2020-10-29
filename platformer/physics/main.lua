a09341ec26ce272a1dbfa22c2d23a70fc7f4dcf4
dylan <dylanw2u19@gmail.com>
2020-10-29 12:06:26 +0000

    completed worldSetup

---
+++ platformer/physics/main.lua
@@ -0,0 +1,26 @@
+function love.load()
+    -- wf represents windfield code cloned in
+    wf = require 'libraries/windfield/windfield'
+
+    -- params represent gravity for the physics world, x and y
+    world = wf.newWorld(0, 100)
+
+    -- x,y, width and height
+    player = world:newRectangleCollider(360, 100, 80, 80)
+
+    -- making a platform for the player to landon
+    platform = world:newRectangleCollider(250, 400, 300, 100)
+
+    -- static means it will not move
+    platform:setType('static')
+end
+
+
+function love.update(dt)
+    world:update(dt)
+end
+
+
+function love.draw()
+    world:draw()
+end
